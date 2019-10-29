17aae5c6533339cea1d3b2df259f545ce112274a
Anthhony <abuehler@openwt.com>
2019-10-29 16:36:08 +0100

    Revert after practicing databinding with username

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    ServerComponent,
+    ServersComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     NgbModule,
     BrowserAnimationsModule
