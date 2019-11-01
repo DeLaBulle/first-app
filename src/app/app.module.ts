51af773ab8f44050c3b974c33807cb2646d04e2d
Anthhony <abuehler@openwt.com>
2019-11-01 06:40:07 +0100

    Return to clean and empty install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
   ],
   imports: [
     FormsModule,
